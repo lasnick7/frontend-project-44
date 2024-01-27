7f425018e63e9ebc19160111db20508f8f76cd32
lasnick7 <classmate1@yandex.com>
2024-01-27 12:00:21 +0300

    step 5

---
+++ bin/brain-even.js
@@ -0,0 +1,7 @@
+
+import { welcomeUser } from '../src/cli.js';
+import { askQuestion } from '../src/cli.js';
+import { checkAnswer } from '../src/cli.js';
+import { playGame } from '../src/cli.js';
+
+playGame();
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
-
 import { welcomeUser } from '../src/cli.js';
 
 welcomeUser();
