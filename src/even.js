3bfef3d11e73ffcd3c3599b62eb43efa1a89a454
Nikita Laskovykh <lasnick711@gmail.com>
2024-12-15 18:26:15 +0300

    all done???

--- src/even.js
+++ src/even.js
@@ -1,14 +1,11 @@
 import readlineSync from 'readline-sync';
-
 var name;
 function welcomeUser() {
   console.log('Welcome to the Brain Games!');
   name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 }
-
 export { welcomeUser };
-
 var answer;
 var randomNumber;
 function askQuestion() {
@@ -17,46 +14,30 @@ function askQuestion() {
   answer = readlineSync.question(`Question: ${randomNumber}\nYour answer: `);
   return answer;
 }
-
 export { askQuestion };
-
 function checkAnswer(answer) {
-  const isEven = (number) => {
-    if (number % 2 === 0) {
-      return true;
-    }
+  if (answer === 'yes' && randomNumber % 2 === 0) {
+    console.log('Correct!');
+    return true;
+  } if (answer === 'no' && randomNumber % 2 === 0) {
+    // eslint-disable-next-line no-template-curly-in-string
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was \'yes\'.`);
+    console.log(`Let's try again, ${name}!`);
     return false;
-  }
-  if (isEven(randomNumber) === true) {
-    if (answer === 'yes') {
-      console.log('Correct!');
-      return true;
-    } else if (answer === 'no') {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was \'yes\'.`);
-      console.log(`Let's try again, ${name}!`);
-      return false;
-    } else {
-      console.log(`'${answer}' is wrong answer ;(.`);
-      console.log(`Let's try again, ${name}!`);
-    }
-  }
-  if (isEven(randomNumber) === false) {
-    if (answer === 'no') {
-      console.log('Correct!');
-      return true;
-    } else if (answer === 'yes') {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was \'no\'.`);
-      console.log(`Let's try again, ${name}!`);
-      return false;
-    } else {
-      console.log(`'${answer}' is wrong answer ;(.`);
-      console.log(`Let's try again, ${name}!`);
-    }
+  } if (answer === 'no' && randomNumber % 2 !== 0) {
+    console.log('Correct!');
+    return true;
+  } if (answer === 'yes' && randomNumber % 2 !== 0) {
+    // eslint-disable-next-line no-template-curly-in-string
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was \'no\'.`);
+    console.log(`Let's try again, ${name}!`);
+    return false;
+  } else {
+    console.log(`'${answer}' is wrong answer ;(.`);
+    console.log(`Let's try again, ${name}!`);
   }
 }
-
 export { checkAnswer };
-
 function playGame() {
   let winCount = 0;
   while (winCount < 3) {
@@ -73,5 +54,4 @@ function playGame() {
   // eslint-disable-next-line no-restricted-globals, no-undef
   console.log(`Congratulations, ${name}!`);
 }
-
 export { playGame };
