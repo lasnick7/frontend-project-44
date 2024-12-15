b6cd8ca2e49c84d3cde8e5d5ad46799a8f3465b4
Nikita Laskovykh <lasnick711@gmail.com>
2024-12-15 19:02:04 +0300

    upd calc.js

--- src/calc.js
+++ src/calc.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-var name;
+let name;
 function welcomeUser() {
   console.log('Welcome to the Brain Games!');
   name = readlineSync.question('May I have your name? ');
