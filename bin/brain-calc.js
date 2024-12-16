f91a74dda8307c18da1fd5f80c3179e20e657cbc
Nikita Laskovykh <lasnick711@gmail.com>
2024-12-16 13:00:54 +0300

    some more fix

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 import {
-  wellcomeUser, askQuestion, checkAnswer, name,
+  wellcomeUser, askQuestion, checkAnswer,
 } from '../src/games/calc.js';
 import playGame from '../src/playGame.js';
 
-console.log(name);
-playGame(wellcomeUser, askQuestion, checkAnswer, name);
-console.log(name);
+playGame(wellcomeUser, askQuestion, checkAnswer);
--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import {
-  wellcomeUser, askQuestion, checkAnswer, name,
+  wellcomeUser, askQuestion, checkAnswer,
 } from '../src/games/gcd.js';
 import playGame from '../src/playGame.js';
 
-playGame(wellcomeUser, askQuestion, checkAnswer, name);
+playGame(wellcomeUser, askQuestion, checkAnswer);
